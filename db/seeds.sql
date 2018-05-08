5354f4a2912370ef837782d1b99e662fb1e5c11e
Luke Myers <lukemyers0921@gmail.com>
2018-05-07 21:06:26 -0400

    reuploaded

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('Mushroom and Swiss', false);
+INSERT INTO burgers (name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Black Bean Burger', false);
